691befadf4ca791cea6c42793a711c874c45888e
Nico <nico-2006@web.de>
2015-01-17 10:21:54 +0100

    Moved USB_EP_SIZE definition
    This was needed in order to use the HID_Core without an USB AVR to still
    use the HID APIs

--- avr/cores/hid/USB-Core/USBAPI.h
+++ avr/cores/hid/USB-Core/USBAPI.h
@@ -52,6 +52,13 @@ typedef unsigned long u32;
 
 #include "Arduino.h"
 
+// edit by NicoHood
+// this definitions is usefull if you want to reduce the EP_SIZE to 16
+// at the moment only 64 and 16 as EP_SIZE for all EPs are supported except the control endpoint
+#ifndef USB_EP_SIZE
+#define USB_EP_SIZE 64
+#endif
+
 #if defined(USBCON)
 
 #include "USBDesc.h"
