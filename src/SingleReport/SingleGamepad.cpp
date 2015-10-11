c8cb67ebd614b371d0732b2de7d37b02802a52fb
NicoHood <NicoHood@users.noreply.github.com>
2015-10-11 11:08:37 +0200

    Fixed Gamepad compile errors

--- src/SingleReport/SingleGamepad.cpp
+++ src/SingleReport/SingleGamepad.cpp
@@ -21,7 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include "Gamepad.h"
+#include "SingleGamepad.h"
 
 static const uint8_t _hidReportDescriptorGamepad[] PROGMEM = {
 	/* Gamepad with 32 buttons and 6 axis*/
