eed1098a218496051973968712af2d0921e69c1a
NicoHood <NicoHood@users.noreply.github.com>
2015-11-08 00:13:42 +0100

    Added featureReport data to constructor

--- src/SingleReport/BootKeyboard.cpp
+++ src/SingleReport/BootKeyboard.cpp
@@ -71,7 +71,7 @@ static const uint8_t _hidReportDescriptorKeyboard[] PROGMEM = {
     0xc0                            /* END_COLLECTION */
 };
 
-BootKeyboard_::BootKeyboard_(void) : PluggableUSBModule(1, 1, epType), protocol(HID_REPORT_PROTOCOL), idle(1), leds(0)
+BootKeyboard_::BootKeyboard_(void) : PluggableUSBModule(1, 1, epType), protocol(HID_REPORT_PROTOCOL), idle(1), leds(0), featureReport(NULL), featureLength(0)
 {
 	epType[0] = EP_TYPE_INTERRUPT_IN;
 	PluggableUSB().plug(this);
