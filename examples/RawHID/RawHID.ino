c197491272e7da56d659b8342c9c21602e7e4bfb
NicoHood <NicoHood@users.noreply.github.com>
2015-11-08 20:05:53 +0100

    Updated RawHID with new, more intuitive API

--- examples/RawHID/RawHID.ino
+++ examples/RawHID/RawHID.ino
@@ -7,11 +7,7 @@
   Shows how to send bytes via RawHID.
   Press a button to send some example values.
 
-  Every received data is mirrored to the host.
-  The use of the RawHIDEvent() function is shown too.
-
-  This sketch only tries to show the possiblities
-  and is not perfect.You might want to use RawHID differently.
+  Every received data is mirrored to the host via Serial.
 
   See HID Project documentation for more information.
   https://github.com/NicoHood/HID/wiki/RawHID-API
@@ -22,14 +18,22 @@
 const int pinLed = LED_BUILTIN;
 const int pinButton = 2;
 
-uint8_t data[255];
-volatile size_t len = 0;
+// Buffer to hold RawHID data.
+// If host tries to send more data than this,
+// it will respond with an error.
+// If the data is not read until the host sends the next data
+// it will also respond with an error and the data will be lost.
+uint8_t rawhidData[255];
 
 void setup() {
   pinMode(pinLed, OUTPUT);
   pinMode(pinButton, INPUT_PULLUP);
 
-  // No begin/end function required for RawHID
+  Serial.begin(115200);
+
+  // Set the RawHID OUT report array.
+  // Feature reports are also (parallel) possible, see the other example for this.
+  RawHID.begin(rawhidData, sizeof(rawhidData));
 }
 
 void loop() {
@@ -50,54 +54,18 @@ void loop() {
   }
 
 
-  // This will miss longer RawHID data transmissions
-  // and return an error to the host if data was missed.
-  // Only use this for non changing/less important data.
-  // Or you can use this if the event aborted on a full buffer.
-  // Please note, that all data after this full buffer is missed anyways.
+  // Check if there is new data from the RawHID device
   auto bytesAvailable = RawHID.available();
-  while (bytesAvailable--) {
-    if (len < sizeof(data)) {
-      data[len++] = RawHID.read();
-    }
-  }
-
-  // Process data from the host
-  if (len) {
+  if (bytesAvailable)
+  {
     digitalWrite(pinLed, HIGH);
 
-    // Disable interrupts while processing the data
-    uint8_t oldSREG = SREG;
-    cli();
-
-    // Mirror the incoming data from the host back
-    RawHID.write(data, len);
-    len = 0;
-
-    SREG = oldSREG;
-
-    // Simple debounce for led
-    delay(300);
-    digitalWrite(pinLed, LOW);
-  }
-}
-
-void RawHIDEvent(uint8_t num) {
-  // This event is called via interrupt.
-  // Do not use print inside, or other long function calls!
-  // If you not use this event function,
-  // you might miss some data in the loop,
-  // if the host sends too fast or too much data at once.
-
-  auto bytesAvailable = RawHID.available();
-  while (bytesAvailable--) {
-    // Only add data to the buffer if its not full.
-    // If it is, no more event will occur
-    // and the data should be discarded
-    // or read (as shown) in the loop above.
-    if (len < sizeof(data)) {
-      data[len++] = RawHID.read();
+    // Mirror data via Serial
+    while (bytesAvailable--) {
+      Serial.println(RawHID.read());
     }
+
+    digitalWrite(pinLed, LOW);
   }
 }
 
