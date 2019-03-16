cbccdbcda41363e9d4c9cec8f9c41f1ee9a7e682
David Wischnjak <davidw.software@gmail.com>
2019-03-17 00:16:23 +0100

    Added german layout

--- src/KeyboardLayouts/ImprovedKeylayoutsDE.h
+++ src/KeyboardLayouts/ImprovedKeylayoutsDE.h
@@ -24,59 +24,10 @@ THE SOFTWARE.
 // Include guard
 #pragma once
 
-// Keyboard Modifiers
-enum KeyboardMods : uint16_t {
-	MOD_LEFT_CTRL		= 0b0000000100000000,
-	MOD_LEFT_SHIFT		= 0b0000001000000000,
-	MOD_LEFT_ALT		= 0b0000010000000000,
-	MOD_LEFT_GUI		= 0b0000100000000000,
-	MOD_RIGHT_CTRL		= 0b0001000000000000,
-	MOD_RIGHT_SHIFT		= 0b0010000000000000,
-	MOD_RIGHT_ALT		= 0b0100000000000000,
-	MOD_RIGHT_GUI		= 0b1000000000000000,
-};
-
-// Keyboard Leds
-enum KeyboardLeds : uint8_t {
-	LED_NUM_LOCK		= (1 << 0),
-	LED_CAPS_LOCK		= (1 << 1),
-	LED_SCROLL_LOCK		= (1 << 2),
-	LED_COMPOSE			= (1 << 3),
-	LED_KANA			= (1 << 4),
-	LED_POWER			= (1 << 5),
-	LED_SHIFT			= (1 << 6),
-	LED_DO_NOT_DISTURB	= (1 << 7),
-};
-
-#if defined(LAYOUT_US_ENGLISH)
-	//#include "ImprovedKeylayoutsUS.h"
-#elif defined(LAYOUT_CANADIAN_FRENCH)
-#elif defined(LAYOUT_CANADIAN_MULTILINGUAL)
-#elif defined(LAYOUT_DANISH)
-#elif defined(LAYOUT_FINNISH)
-#elif defined(LAYOUT_FRENCH)
-#elif defined(LAYOUT_FRENCH_BELGIAN)
-#elif defined(LAYOUT_FRENCH_SWISS)
-#elif defined(LAYOUT_GERMAN)
-	#include "ImprovedKeylayoutsDE.h"
-#elif defined(LAYOUT_GERMAN_MAC)
-#elif defined(LAYOUT_GERMAN_SWISS)
-#elif defined(LAYOUT_ICELANDIC)
-#elif defined(LAYOUT_IRISH)
-#elif defined(LAYOUT_ITALIAN)
-#elif defined(LAYOUT_NORWEGIAN)
-#elif defined(LAYOUT_PORTUGUESE)
-#elif defined(LAYOUT_PORTUGUESE_BRAZILIAN)
-#elif defined(LAYOUT_SPANISH)
-#elif defined(LAYOUT_SPANISH_LATIN_AMERICA)
-#elif defined(LAYOUT_SWEDISH)
-#elif defined(LAYOUT_TURKISH)
-#elif defined(LAYOUT_UNITED_KINGDOM)
-#elif defined(LAYOUT_US_INTERNATIONAL)
-#endif
+#include "ImprovedKeylayouts.h"
 
 // Hut1_12v2.pdf
-/*
+
 enum KeyboardKeycode : uint8_t {
 	KEY_RESERVED		=  0,
 	KEY_ERROR_ROLLOVER	=  1,
@@ -106,8 +57,8 @@ enum KeyboardKeycode : uint8_t {
     KEY_V               = 25,
     KEY_W               = 26,
     KEY_X               = 27,
-    KEY_Y               = 28,
-    KEY_Z               = 29,
+    KEY_Y               = 29,
+    KEY_Z               = 28,
     KEY_1               = 30,
     KEY_2               = 31,
     KEY_3               = 32,
@@ -124,18 +75,18 @@ enum KeyboardKeycode : uint8_t {
     KEY_BACKSPACE       = 42,
     KEY_TAB             = 43,
     KEY_SPACE           = 44,
-    KEY_MINUS           = 45,
-    KEY_EQUAL           = 46,
-    KEY_LEFT_BRACE      = 47,
-    KEY_RIGHT_BRACE     = 48,
-    KEY_BACKSLASH       = 49,
-    KEY_NON_US_NUM      = 50,
-    KEY_SEMICOLON       = 51,
-    KEY_QUOTE           = 52,
-    KEY_TILDE           = 53,
+    KEY_SZ              = 45,
+    KEY_ACCENT          = 46,
+    KEY_UE              = 47,
+    KEY_PLUS            = 48,
+    KEY_HASHTAG2        = 49,
+    KEY_HASHTAG         = 50,
+    KEY_OE              = 51,
+    KEY_AE              = 52,
+    KEY_DACH            = 53,
     KEY_COMMA           = 54,
     KEY_PERIOD          = 55,
-    KEY_SLASH           = 56,
+    KEY_MINUS           = 56,
     KEY_CAPS_LOCK		= 0x39,
     KEY_F1				= 0x3A,
     KEY_F2				= 0x3B,
@@ -184,7 +135,7 @@ enum KeyboardKeycode : uint8_t {
     KEYPAD_9 			= 0x61,
     KEYPAD_0 			= 0x62,
     KEYPAD_DOT			= 0x63,
-    KEY_NON_US			= 0x64,
+    KEY_SMALLER			= 0x64,
     KEY_APPLICATION		= 0x65, // Context menu/right click
     KEY_MENU            = 0x65, // Alias
     
@@ -315,171 +266,6 @@ enum KeyboardKeycode : uint8_t {
 	
     // Keyboard HID mappings
 
-    //  Reserved (no_event_indicated)
-    HID_KEYBOARD_ERROR_ROLLOVER	= 0x01,
-    HID_KEYBOARD_POST_FAIL	= 0x02,
-    HID_KEYBOARD_ERROR_UNDEFINED	= 0x03,
-    HID_KEYBOARD_A_AND_A	= 0x04,
-    HID_KEYBOARD_B_AND_B	= 0x05,
-    HID_KEYBOARD_C_AND_C	= 0x06,
-    HID_KEYBOARD_D_AND_D	= 0x07,
-    HID_KEYBOARD_E_AND_E	= 0x08,
-    HID_KEYBOARD_F_AND_F	= 0x09,
-    HID_KEYBOARD_G_AND_G	= 0x0A,
-    HID_KEYBOARD_H_AND_H	= 0x0B,
-    HID_KEYBOARD_I_AND_I	= 0x0C,
-    HID_KEYBOARD_J_AND_J	= 0x0D,
-    HID_KEYBOARD_K_AND_K	= 0x0E,
-    HID_KEYBOARD_L_AND_L	= 0x0F,
-    HID_KEYBOARD_M_AND_M	= 0x10,
-    HID_KEYBOARD_N_AND_N	= 0x11,
-    HID_KEYBOARD_O_AND_O	= 0x12,
-    HID_KEYBOARD_P_AND_P	= 0x13,
-    HID_KEYBOARD_Q_AND_Q	= 0x14,
-    HID_KEYBOARD_R_AND_R	= 0x15,
-    HID_KEYBOARD_S_AND_S	= 0x16,
-    HID_KEYBOARD_T_AND_T	= 0x17,
-    HID_KEYBOARD_U_AND_U	= 0x18,
-    HID_KEYBOARD_V_AND_V	= 0x19,
-    HID_KEYBOARD_W_AND_W	= 0x1A,
-    HID_KEYBOARD_X_AND_X	= 0x1B,
-    HID_KEYBOARD_Y_AND_Y	= 0x1C,
-    HID_KEYBOARD_Z_AND_Z	= 0x1D,
-    HID_KEYBOARD_1_AND_EXCLAMATION_POINT	= 0x1E,
-    HID_KEYBOARD_2_AND_AT	= 0x1F,
-    HID_KEYBOARD_3_AND_POUND	= 0x20,
-    HID_KEYBOARD_4_AND_DOLLAR	= 0x21,
-    HID_KEYBOARD_5_AND_PERCENT	= 0x22,
-    HID_KEYBOARD_6_AND_CARAT	= 0x23,
-    HID_KEYBOARD_7_AND_AMPERSAND	= 0x24,
-    HID_KEYBOARD_8_AND_ASTERISK	= 0x25,
-    HID_KEYBOARD_9_AND_LEFT_PAREN	= 0x26,
-    HID_KEYBOARD_0_AND_RIGHT_PAREN	= 0x27,
-    HID_KEYBOARD_ENTER	= 0x28,	 // (MARKED AS ENTER_SLASH_RETURN)
-    HID_KEYBOARD_ESCAPE	= 0x29,
-    HID_KEYBOARD_DELETE	= 0x2A,	// (BACKSPACE)
-    HID_KEYBOARD_TAB	= 0x2B,
-    HID_KEYBOARD_SPACEBAR	= 0x2C,
-    HID_KEYBOARD_MINUS_AND_UNDERSCORE	= 0x2D,	 // (UNDERSCORE)
-    HID_KEYBOARD_EQUALS_AND_PLUS	= 0x2E,
-    HID_KEYBOARD_LEFT_BRACKET_AND_LEFT_CURLY_BRACE	= 0x2F,
-    HID_KEYBOARD_RIGHT_BRACKET_AND_RIGHT_CURLY_BRACE	= 0x30,
-    HID_KEYBOARD_BACKSLASH_AND_PIPE	= 0x31,
-    HID_KEYBOARD_NON_US_POUND_AND_TILDE	= 0x32,
-    HID_KEYBOARD_SEMICOLON_AND_COLON	= 0x33,
-    HID_KEYBOARD_QUOTE_AND_DOUBLEQUOTE	= 0x34,
-    HID_KEYBOARD_GRAVE_ACCENT_AND_TILDE	= 0x35,
-    HID_KEYBOARD_COMMA_AND_LESS_THAN	= 0x36,
-    HID_KEYBOARD_PERIOD_AND_GREATER_THAN	= 0x37,
-    HID_KEYBOARD_SLASH_AND_QUESTION_MARK	= 0x38,
-    HID_KEYBOARD_CAPS_LOCK	= 0x39,
-    HID_KEYBOARD_F1	= 0x3A,
-    HID_KEYBOARD_F2	= 0x3B,
-    HID_KEYBOARD_F3	= 0x3C,
-    HID_KEYBOARD_F4	= 0x3D,
-    HID_KEYBOARD_F5	= 0x3E,
-    HID_KEYBOARD_F6	= 0x3F,
-    HID_KEYBOARD_F7	= 0x40,
-    HID_KEYBOARD_F8	= 0x41,
-    HID_KEYBOARD_F9	= 0x42,
-    HID_KEYBOARD_F10	= 0x43,
-    HID_KEYBOARD_F11	= 0x44,
-    HID_KEYBOARD_F12	= 0x45,
-    HID_KEYBOARD_PRINTSCREEN	= 0x46,
-    HID_KEYBOARD_SCROLL_LOCK	= 0x47,
-    HID_KEYBOARD_PAUSE	= 0x48,
-    HID_KEYBOARD_INSERT	= 0x49,
-    HID_KEYBOARD_HOME	= 0x4A,
-    HID_KEYBOARD_PAGE_UP	= 0x4B,
-    HID_KEYBOARD_DELETE_FORWARD	= 0x4C,
-    HID_KEYBOARD_END	= 0x4D,
-    HID_KEYBOARD_PAGE_DOWN	= 0x4E,
-    HID_KEYBOARD_RIGHTARROW	= 0x4F,
-    HID_KEYBOARD_LEFTARROW	= 0x50,
-    HID_KEYBOARD_DOWNARROW	= 0x51,
-    HID_KEYBOARD_UPARROW	= 0x52,
-    HID_KEYPAD_NUM_LOCK_AND_CLEAR	= 0x53,
-    HID_KEYPAD_DIVIDE	= 0x54,
-    HID_KEYPAD_MULTIPLY	= 0x55,
-    HID_KEYPAD_SUBTRACT	= 0x56,
-    HID_KEYPAD_ADD	= 0x57,
-    HID_KEYPAD_ENTER	= 0x58,
-    HID_KEYPAD_1_AND_END	= 0x59,
-    HID_KEYPAD_2_AND_DOWN_ARROW	= 0x5A,
-    HID_KEYPAD_3_AND_PAGE_DOWN	= 0x5B,
-    HID_KEYPAD_4_AND_LEFT_ARROW	= 0x5C,
-    HID_KEYPAD_5	= 0x5D,
-    HID_KEYPAD_6_AND_RIGHT_ARROW	= 0x5E,
-    HID_KEYPAD_7_AND_HOME	= 0x5F,
-    HID_KEYPAD_8_AND_UP_ARROW	= 0x60,
-    HID_KEYPAD_9_AND_PAGE_UP	= 0x61,
-    HID_KEYPAD_0_AND_INSERT	= 0x62,
-    HID_KEYPAD_PERIOD_AND_DELETE	= 0x63,
-    HID_KEYBOARD_NON_US_BACKSLASH_AND_PIPE	= 0x64,
-    HID_KEYBOARD_APPLICATION	= 0x65,
-    HID_KEYBOARD_POWER	= 0x66,
-    HID_KEYPAD_EQUALS	= 0x67,
-    HID_KEYBOARD_F13	= 0x68,
-    HID_KEYBOARD_F14	= 0x69,
-    HID_KEYBOARD_F15	= 0x6A,
-    HID_KEYBOARD_F16	= 0x6B,
-    HID_KEYBOARD_F17	= 0x6C,
-    HID_KEYBOARD_F18	= 0x6D,
-    HID_KEYBOARD_F19	= 0x6E,
-    HID_KEYBOARD_F20	= 0x6F,
-    HID_KEYBOARD_F21	= 0x70,
-    HID_KEYBOARD_F22	= 0x71,
-    HID_KEYBOARD_F23	= 0x72,
-    HID_KEYBOARD_F24	= 0x73,
-    HID_KEYBOARD_EXECUTE	= 0x74,
-    HID_KEYBOARD_HELP	= 0x75,
-    HID_KEYBOARD_MENU	= 0x76,
-    HID_KEYBOARD_SELECT	= 0x77,
-    HID_KEYBOARD_STOP	= 0x78,
-    HID_KEYBOARD_AGAIN	= 0x79,
-    HID_KEYBOARD_UNDO	= 0x7A,
-    HID_KEYBOARD_CUT	= 0x7B,
-    HID_KEYBOARD_COPY	= 0x7C,
-    HID_KEYBOARD_PASTE	= 0x7D,
-    HID_KEYBOARD_FIND	= 0x7E,
-    HID_KEYBOARD_MUTE	= 0x7F,
-    HID_KEYBOARD_VOLUME_UP	= 0x80,
-    HID_KEYBOARD_VOLUME_DOWN	= 0x81,
-    HID_KEYBOARD_LOCKING_CAPS_LOCK	= 0x82,
-    HID_KEYBOARD_LOCKING_NUM_LOCK	= 0x83,
-    HID_KEYBOARD_LOCKING_SCROLL_LOCK	= 0x84,
-    HID_KEYPAD_COMMA	= 0x85,
-    HID_KEYPAD_EQUAL_SIGN	= 0x86,
-    HID_KEYBOARD_INTERNATIONAL1	= 0x87,
-    HID_KEYBOARD_INTERNATIONAL2	= 0x88,
-    HID_KEYBOARD_INTERNATIONAL3	= 0x89,
-    HID_KEYBOARD_INTERNATIONAL4	= 0x8A,
-    HID_KEYBOARD_INTERNATIONAL5	= 0x8B,
-    HID_KEYBOARD_INTERNATIONAL6	= 0x8C,
-    HID_KEYBOARD_INTERNATIONAL7	= 0x8D,
-    HID_KEYBOARD_INTERNATIONAL8	= 0x8E,
-    HID_KEYBOARD_INTERNATIONAL9	= 0x8F,
-    HID_KEYBOARD_LANG1	= 0x90,
-    HID_KEYBOARD_LANG2	= 0x91,
-    HID_KEYBOARD_LANG3	= 0x92,
-    HID_KEYBOARD_LANG4	= 0x93,
-    HID_KEYBOARD_LANG5	= 0x94,
-    HID_KEYBOARD_LANG6	= 0x95,
-    HID_KEYBOARD_LANG7	= 0x96,
-    HID_KEYBOARD_LANG8	= 0x97,
-    HID_KEYBOARD_LANG9	= 0x98,
-    HID_KEYBOARD_ALTERNATE_ERASE	= 0x99,
-    HID_KEYBOARD_SYSREQ_SLASH_ATTENTION	= 0x9A,
-    HID_KEYBOARD_CANCEL	= 0x9B,
-    HID_KEYBOARD_CLEAR	= 0x9C,
-    HID_KEYBOARD_PRIOR	= 0x9D,
-    HID_KEYBOARD_RETURN	= 0x9E,
-    HID_KEYBOARD_SEPARATOR	= 0x9F,
-    HID_KEYBOARD_OUT	= 0xA0,
-    HID_KEYBOARD_OPER	= 0xA1,
-    HID_KEYBOARD_CLEAR_SLASH_AGAIN	= 0xA2,
-    HID_KEYBOARD_CRSEL_SLASH_PROPS	= 0xA3,
-    HID_KEYBOARD_EXSEL	= 0xA4,
     // Reserved	0xA5-AF
     HID_KEYPAD_00	= 0xB0,
     HID_KEYPAD_000	= 0xB1,
@@ -538,9 +324,9 @@ enum KeyboardKeycode : uint8_t {
     HID_KEYBOARD_RIGHT_ALT	= 0xE6,
     HID_KEYBOARD_RIGHT_GUI	= 0xE7,
 };
-*/
 
-/*
+
+
 static const uint16_t _asciimap[] PROGMEM =
 {
 	KEY_RESERVED,           // NUL
@@ -577,21 +363,21 @@ static const uint16_t _asciimap[] PROGMEM =
 	KEY_RESERVED,           // US 
 
 	KEY_SPACE,		   		// ' ' Space
-	KEY_1|SHIFT,	   		// !
-	KEY_QUOTE|SHIFT,	   	// "
-	KEY_3|SHIFT,    		// #
-	KEY_4|SHIFT,    		// $
-	KEY_5|SHIFT,    		// %
-	KEY_7|SHIFT,    		// &
-	KEY_QUOTE,          	// '
-	KEY_9|SHIFT,    		// (
-	KEY_0|SHIFT,    		// )
-	KEY_8|SHIFT,    		// *
-	KEY_EQUAL|SHIFT,    	// +
+	KEY_1|MOD_LEFT_SHIFT,			// !
+	KEY_2|MOD_LEFT_SHIFT,			// "
+	KEY_HASHTAG,		    		// #
+	KEY_4|MOD_LEFT_SHIFT,    		// $
+	KEY_5|MOD_LEFT_SHIFT,    		// %
+	KEY_6|MOD_LEFT_SHIFT,    		// &
+	KEY_HASHTAG|MOD_LEFT_SHIFT,     // '
+	KEY_8|MOD_LEFT_SHIFT,    		// (
+	KEY_9|MOD_LEFT_SHIFT,    		// )
+	KEY_PLUS|MOD_LEFT_SHIFT,    	// *
+	KEY_PLUS,    					// +
 	KEY_COMMA,          	// ,
 	KEY_MINUS,          	// -
 	KEY_PERIOD,          	// .
-	KEY_SLASH,          	// /
+	KEY_7|MOD_LEFT_SHIFT,   // /
 	KEY_0,          		// 0
 	KEY_1,          		// 1
 	KEY_2,          		// 2
@@ -602,45 +388,45 @@ static const uint16_t _asciimap[] PROGMEM =
 	KEY_7,          		// 7
 	KEY_8,          		// 8
 	KEY_9,          		// 9
-	KEY_SEMICOLON|SHIFT,	// :
-	KEY_SEMICOLON,          // ;
-	KEY_COMMA|SHIFT,      	// <
-	KEY_EQUAL,          	// =
-	KEY_PERIOD|SHIFT,      	// >
-	KEY_SLASH|SHIFT,      	// ?
-	KEY_2|SHIFT,      		// @
-	KEY_A|SHIFT,      		// A
-	KEY_B|SHIFT,      		// B
-	KEY_C|SHIFT,      		// C
-	KEY_D|SHIFT,      		// D
-	KEY_E|SHIFT,      		// E
-	KEY_F|SHIFT,      		// F
-	KEY_G|SHIFT,      		// G
-	KEY_H|SHIFT,      		// H
-	KEY_I|SHIFT,      		// I
-	KEY_J|SHIFT,      		// J
-	KEY_K|SHIFT,      		// K
-	KEY_L|SHIFT,      		// L
-	KEY_M|SHIFT,      		// M
-	KEY_N|SHIFT,      		// N
-	KEY_O|SHIFT,      		// O
-	KEY_P|SHIFT,      		// P
-	KEY_Q|SHIFT,      		// Q
-	KEY_R|SHIFT,      		// R
-	KEY_S|SHIFT,      		// S
-	KEY_T|SHIFT,      		// T
-	KEY_U|SHIFT,      		// U
-	KEY_V|SHIFT,      		// V
-	KEY_W|SHIFT,      		// W
-	KEY_X|SHIFT,      		// X
-	KEY_Y|SHIFT,      		// Y
-	KEY_Z|SHIFT,      		// Z
-	KEY_LEFT_BRACE,         // [
-	KEY_BACKSLASH,          // bslash
-	KEY_RIGHT_BRACE,        // ]
-	KEY_6|SHIFT,    		// ^
-	KEY_MINUS|SHIFT,    	// _
-	KEY_TILDE,          	// `
+	KEY_PERIOD|MOD_LEFT_SHIFT,	// :
+	KEY_COMMA|MOD_LEFT_SHIFT,   // ;
+	KEY_SMALLER,      			// <
+	KEY_0|MOD_LEFT_SHIFT,          	// =
+	KEY_SMALLER|MOD_LEFT_SHIFT,      // >
+	KEY_SZ|MOD_LEFT_SHIFT,      	// ?
+	KEY_Q|MOD_RIGHT_ALT,      		// @
+	KEY_A|MOD_LEFT_SHIFT,      		// A
+	KEY_B|MOD_LEFT_SHIFT,      		// B
+	KEY_C|MOD_LEFT_SHIFT,      		// C
+	KEY_D|MOD_LEFT_SHIFT,      		// D
+	KEY_E|MOD_LEFT_SHIFT,      		// E
+	KEY_F|MOD_LEFT_SHIFT,      		// F
+	KEY_G|MOD_LEFT_SHIFT,      		// G
+	KEY_H|MOD_LEFT_SHIFT,      		// H
+	KEY_I|MOD_LEFT_SHIFT,      		// I
+	KEY_J|MOD_LEFT_SHIFT,      		// J
+	KEY_K|MOD_LEFT_SHIFT,      		// K
+	KEY_L|MOD_LEFT_SHIFT,      		// L
+	KEY_M|MOD_LEFT_SHIFT,      		// M
+	KEY_N|MOD_LEFT_SHIFT,      		// N
+	KEY_O|MOD_LEFT_SHIFT,      		// O
+	KEY_P|MOD_LEFT_SHIFT,      		// P
+	KEY_Q|MOD_LEFT_SHIFT,      		// Q
+	KEY_R|MOD_LEFT_SHIFT,      		// R
+	KEY_S|MOD_LEFT_SHIFT,      		// S
+	KEY_T|MOD_LEFT_SHIFT,      		// T
+	KEY_U|MOD_LEFT_SHIFT,      		// U
+	KEY_V|MOD_LEFT_SHIFT,      		// V
+	KEY_W|MOD_LEFT_SHIFT,      		// W
+	KEY_X|MOD_LEFT_SHIFT,      		// X
+	KEY_Y|MOD_LEFT_SHIFT,      		// Y
+	KEY_Z|MOD_LEFT_SHIFT,      		// Z
+	KEY_8|MOD_RIGHT_ALT,	         // [
+	KEY_SZ|MOD_RIGHT_ALT,          // bslash
+	KEY_9|MOD_RIGHT_ALT,        	// ]
+	KEY_6|MOD_LEFT_SHIFT,    		// ^
+	KEY_MINUS|MOD_LEFT_SHIFT,    	// _
+	KEY_ACCENT|MOD_LEFT_SHIFT,      // `
 	KEY_A,          		// a
 	KEY_B,          		// b
 	KEY_C,          		// c
@@ -667,10 +453,10 @@ static const uint16_t _asciimap[] PROGMEM =
 	KEY_X,          		// x
 	KEY_Y,          		// y
 	KEY_Z,          		// z
-	KEY_LEFT_BRACE|SHIFT,   // {
-	KEY_BACKSLASH|SHIFT,    // |
-	KEY_RIGHT_BRACE|SHIFT,  // }
-	KEY_TILDE|SHIFT,    	// ~
+	KEY_7|MOD_RIGHT_ALT,	// {
+	KEY_SMALLER|MOD_RIGHT_ALT,    		// |
+	KEY_0|MOD_RIGHT_ALT,	// }
+	KEY_PLUS|MOD_RIGHT_ALT,    	// ~
 	KEY_RESERVED			// DEL
 };
-*/
+
