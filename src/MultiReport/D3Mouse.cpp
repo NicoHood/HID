fa53506e1ffdeb672c7fedb57e94106c9a45c913
Matyáš Levíček <46134697+Matesaktesak@users.noreply.github.com>
2019-09-23 21:43:44 +0200

    Update and rename 3DMouse.cpp to D3Mouse.cpp

--- src/MultiReport/D3Mouse.cpp
+++ src/MultiReport/D3Mouse.cpp
@@ -1,6 +1,6 @@
-#include "3DMouse.h"
+#include "D3Mouse.h"
 
-static const uint8_t _hidMultiReportDescriptor3DMouse[] PROGMEM = {
+static const uint8_t _hidMultiReportDescriptorD3Mouse[] PROGMEM = {
     0x05, 0x01,       // USAGE_PAGE (Generic Desktop)
     0x09, 0x08,       //   USAGE (Multiaxis)
     0xa1, 0x01,       // COLLECTION (Application)
@@ -20,24 +20,24 @@ static const uint8_t _hidMultiReportDescriptor3DMouse[] PROGMEM = {
     0x09, 0x33,       //     USAGE (Rx)
     0x09, 0x34,       //     USAGE (Ry)
     0x09, 0x35,       //     USAGE (Rz)
-    0x16, 0x00, 0x80, //     LOGICAL_MINIMUM (-32K)
-    0x27, 0x00, 0x80, //     LOGICAL_MAXIMUM (32K)
-    0x75, 0x10,       //     REPORT_SIZE (16)
+    0x16, 0x00, 0xfe, //     LOGICAL_MINIMUM (-512)
+    0x26, 0x00, 0x02, //     LOGICAL_MAXIMUM (512)
+    0x75, 0x0a,       //     REPORT_SIZE (10)
     0x95, 0x06,       //     REPORT_COUNT (6)
     0x81, 0x06,       //     INPUT (Data,Var,Rel)
     0xc0,             //   END_COLLECTION
     0xc0              // END_COLLECTION
 };
 
-3DMouse_::3DMouse_(void)
+D3Mouse_ ::D3Mouse_(void)
 {
-    static HIDSubDescriptor node(_hidMultiReportDescriptor3DMouse, sizeof(_hidMultiReportDescriptor3DMouse));
+    static HIDSubDescriptor node(_hidMultiReportDescriptorD3Mouse, sizeof(_hidMultiReportDescriptorD3Mouse));
     HID().AppendDescriptor(&node);
 }
 
-void 3DMouse_::SendReport(void *data, int length)
+void D3Mouse_ ::SendReport(void *data, int length)
 {
-    HID().SendReport(HID_REPORTID_3DMOUSE, data, length);
+    HID().SendReport(HID_REPORTID_D3MOUSE, data, length);
 }
 
-3DMouse_ 3DMouse;
+D3Mouse_ D3Mouse;
