ce810485cf7afee2df0c0b7d9bccef0c6bba4e96
SukkoPera <software@sukkology.net>
2021-03-04 20:02:23 +0100

    Fix Nico's observations

--- examples/KeymapTest/KeymapTest.ino
+++ examples/KeymapTest/KeymapTest.ino
@@ -1,22 +1,20 @@
 /*
-  Copyright (c) 2014-2015 NicoHood
+  Copyright (c) 2014-2021 NicoHood
   See the readme for credit to other people.
 
-  Consumer example
-  Press a button to play/pause music player
+  KeymapTest Example
+  Test a keymap to make sure that all keys type the character they are ssupposed to
 
-  You may also use SingleConsumer to use a single report.
-
-  See HID Project documentation for more Consumer keys.
-  https://github.com/NicoHood/HID/wiki/Consumer-API
+  See HID Project documentation for more information about the keyboard API:
+  https://github.com/NicoHood/HID/wiki/Keyboard-API
 */
 
 #include <HID-Project.h>
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial)
-    ;
+  while (!Serial) {
+  }
 
   BootKeyboard.begin();
   BootKeyboard.releaseAll();
@@ -26,10 +24,11 @@ void setup() {
   Serial.println ("will be the same on every line. If they are not, try again with a different keymap.");
   Serial.println ("DO NOT CHANGE WINDOW UNTIL THE SKETCH IS DONE!!!");
   Serial.println ("Please click in the input box above, make sure it is empty and press ENTER");
-  while (!Serial.available())
-    ;
-  while (Serial.available())
+  while (!Serial.available()) {
+  }
+  while (Serial.available()) {
     Serial.read ();
+  }
   delay (100);
   
   for (char c = ' '; c <= '~'; ++c) {
@@ -43,11 +42,12 @@ void setup() {
     delay (5);
     BootKeyboard.release(KEY_ENTER);
     delay (5);
-    while (!Serial.available())
-      ;
+    while (!Serial.available()) {
+    }
     char r = Serial.read();
-    while (Serial.available())
+    while (Serial.available()) {
       Serial.read();
+    }
     Serial.print (r);
     if (r == c) {
       Serial.print(" OK");
